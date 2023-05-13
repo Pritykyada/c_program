4d9ecbc82d8285172f943a070ddd2259c13b3bce
pritee <pritikyada22@gmail.com>
2023-05-13 14:05:05 +0530

    commit first

---
+++ Assignment 4.c
@@ -0,0 +1,166 @@
+// Online C compiler to run C program online
+//ASSIGNMENT ---4
+//ITERATIVE CONTROL STATEMENT.........
+//1.WRITE A PROGRAM TO PRINT MySirG 5 TIMES ON THE SCREEN.......
+
+/*#include <stdio.h>
+
+int main() {
+int i=1;
+while(i<=5)
+{
+    printf("MysirG\n");
+    i++;
+}
+    
+    return 0;
+}*/
+//2.WRITE A PROGRAM TO PRINT  THE FIRST 10 NATURAL NUMBERS
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}*/
+
+//3.WRITE A PROGRAM TO PRINT THE FIRST 10 NATURAL NUMBER IN REVERSE ORDER
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+    for(i=10;i<=1;i--)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}*/
+
+//4.WRITE A PROGRAM TO PRINT THE FIRST 10 ODD NATURAL NUMBERS.....
+
+
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+     for(i=1;i<=10*2;i=i+2)
+    {
+        printf("%d\n",i);
+        
+    }
+    return 0;
+   
+     for(int i=1;i<=10;i++)
+    {
+        printf("%d\n",i*2-1);
+        
+    }
+    return 0;
+}*/
+
+//5.WRITE A PROGRAM TO PRINT THE FIRST 10 ODD NATURAL NUMBERS IN REVERSE //ORDER...
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+    for(i=1;i<=10;i=i++)
+    {
+        printf("%d\n",20-i*2-i);
+    }
+    return 0;
+
+    int i;
+     for(i=10;i<=1;i--)
+    {
+        printf("%d\n",i*2-1);
+        
+    }
+    return 0;
+}*/
+
+//6.WRITE A PROGRAM TO PRINT THE FIRST 10 EVEN NATURAL NUMBERS
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+     for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i*2);
+        
+    }
+    return 0;
+
+
+}*/
+
+//7.WRITE A PROGRAM TO PRINT THE FIRST 10 EVEN NATURAL NUMBERS IN REVERSE ORDER...
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+     for(i=10;i>=1;i--)
+    {
+        printf("%d\n",i*2);20,18,16,14,12,10,8,6,4,2
+    }
+    return 0;
+}*/
+
+//8.WRITE A PROGRAM TO PRINT SQUARES OF THE FIRST 10 NATURAL NUMBERS
+
+/*#include <stdio.h>
+
+int main() 
+{
+    int i;
+     for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i*i);
+    }
+    return 0;
+}*/
+
+//9.WRITE A PROGRAM TO PRINT cubes OF THE FIRST 10 NATURAL NUMBERS
+//#include<stdio.h>
+/*int main() 
+{
+    int i;
+     for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i*i*i);
+    }
+    return 0;
+}*/
+
+//10.WRITE A PROGRAM TO PRINT A TABLE OF 5
+/*#include<stdio.h>
+int main() 
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d * %d = %d\n",5,i,5*i);
+    }
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assignment5.c
@@ -0,0 +1,177 @@
+// Online C compiler to run C program online
+//WRITE A PROGRAM TO PRINT MySirG N times on the screen
+
+/*#include <stdio.h>
+int main() {
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        printf("MySirG\n");
+        n--;
+    }
+    return 0;
+}*/
+
+//WRITE A PROGRAM TO PRINT THE FIRST N NATURAL NUMBER......
+/*#include <stdio.h>
+int main() {
+    int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    do
+    {
+        printf("%d ",i);
+        i++;
+    }while(n>=i);
+    return 0;
+
+int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",i);
+        i++;
+    }
+    return 0;
+
+}*/
+
+///WRITE A PROGRAM TO PRINT THE FIRST N NATURAL NUMBER in reverse order
+/*#include <stdio.h>
+int main()
+{
+int n,i=0;
+    printf("enter number");
+    scanf("%d",&n);
+    while(n)
+    {
+        printf("%d ",n);
+        n--;
+    }
+    return 0;
+
+    return 0;
+}*/
+
+//4. Write a program to print the first N odd natural numbers
+
+/*#include <stdio.h>
+int main()
+{
+  int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    while(n*2>i)
+    {
+        printf("%d ",i);
+        i=i+2;
+    }
+    return 0;
+
+    int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",i);
+        i=i+2;
+    }
+}*/
+
+//5. . Write a program to print the first N odd natural numbers in reverse order.
+
+/*#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    if(n%2)
+        {}
+    else
+        n--;
+    while(n>0)
+    {
+        printf("%d ",n);
+        n=n-2;
+    }
+    return 0;
+}*/
+
+//6 Write a program to print the first N even natural numbers
+
+/*#include <stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    for(int i=1;i<=n/2;i++)
+    {
+        printf("%d",i*2);
+    }
+    return 0;
+}*/
+
+//7 Write a program to print the first N even natural numbers in reverse order
+
+/*#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    for(int i=n/2;i>=1;i--)
+    {
+        printf("%d",i*2);
+    }
+}*/
+
+//8 Write a program to print squares of the first N natural numbers
+
+/*#include <stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    while(n>=i)
+    {
+        printf("%d ",i*i);
+        i++;
+    }
+    return 0;
+}*/
+
+//9....... Write a program to print cubes of the first N natural numbers
+/*#include <stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    while(n>=i)
+    {
+        printf("%d ",i*i*i);
+        i++;
+    }
+    return 0;
+}*/
+
+//10.......writea program to print table of n
+/*#include <stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("enter number");
+    scanf("%d",&n);
+    while(10>=i)
+    {
+        printf(" %d * %d = %d\n ",n ,i,n*i);
+        i++;
+    }
+    return 0;
+}*/
